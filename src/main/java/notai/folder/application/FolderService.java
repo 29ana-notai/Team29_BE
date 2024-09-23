b7ec2ee34fdc908964d3d908bf19700e94197be6
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-09-23 21:47:16 +0900

    feat: 루트 폴더 생성하는 기능 구현
    루트 폴더 생성하는 기능 구현

--- src/main/java/notai/folder/application/FolderService.java
+++ src/main/java/notai/folder/application/FolderService.java
@@ -1,9 +1,22 @@
 package notai.folder.application;
 
 import lombok.RequiredArgsConstructor;
+import notai.folder.domain.Folder;
+import notai.folder.domain.FolderRepository;
+import notai.folder.presentation.request.FolderSaveRequest;
+import notai.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FolderService {
+
+    private final FolderRepository folderRepository;
+    private final MemberRepository memberRepository;
+
+    public void saveRootFolder(Long memberId, FolderSaveRequest folderSaveRequest) {
+        var member = memberRepository.getById(memberId);
+        var folder = new Folder(member, folderSaveRequest.name());
+        folderRepository.save(folder);
+    }
 }
--- src/main/java/notai/folder/domain/FolderRepository.java
+++ src/main/java/notai/folder/domain/FolderRepository.java
@@ -1,7 +1,14 @@
 package notai.folder.domain;
 
+import notai.common.exception.type.NotFoundException;
 import notai.folder.query.FolderQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FolderRepository extends JpaRepository<Folder, Long>, FolderQueryRepository {
+
+    default Folder getById(Long id) {
+        return findById(id).orElseThrow(() ->
+                new NotFoundException("폴더 정보를 찾을 수 없습니다.")
+        );
+    }
 }
