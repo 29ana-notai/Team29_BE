248a34da0bd5225ca7efa29f0ae23ae98122dfe6
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-09-24 17:04:28 +0900

    feat: 계층형 구조의 폴더 탐색 기능 구현
    계층형 구조의 폴더 탐색 기능 구현

--- src/main/java/notai/folder/application/FolderQueryService.java
+++ src/main/java/notai/folder/application/FolderQueryService.java
@@ -1,9 +1,65 @@
 package notai.folder.application;
 
 import lombok.RequiredArgsConstructor;
+import notai.folder.application.result.FolderResponse;
+import notai.folder.domain.Folder;
+import notai.folder.domain.FolderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class FolderQueryService {
+
+    private final FolderRepository folderRepository;
+
+    private static final Long ROOT_ID = -1L;
+
+    public List<FolderResponse> getFolders(Long memberId) {
+        var folders = folderRepository.findAllByMemberIdOrderByIdAsc(memberId);
+
+        var subFolderMap = new HashMap<Long, List<Long>>();
+        var folderMap = new HashMap<Long, Folder>();
+        for (var folder : folders) {
+            folderMap.put(folder.getId(), folder);
+            var parentFolderId = folder.getParentFolder() == null
+                    ? ROOT_ID
+                    : folder.getParentFolder().getId();
+            var subFolders = subFolderMap.getOrDefault(parentFolderId, new ArrayList<>());
+            if (folder.getParentFolder() != null) {
+                subFolders.add(folder.getId());
+            }
+            subFolderMap.put(parentFolderId, subFolders);
+        }
+        var result = new ArrayList<FolderResponse>();
+        recursiveInsertFolder(ROOT_ID, subFolderMap, folderMap, result);
+
+        return result;
+    }
+
+    private void recursiveInsertFolder(
+            Long parentFolderId,
+            Map<Long, List<Long>> subFolderMap,
+            Map<Long, Folder> folderMap,
+            List<FolderResponse> result
+    ) {
+        var subFolders = subFolderMap.get(parentFolderId);
+
+        for (var subFolderId : subFolders) {
+            var folder = folderMap.get(subFolderId);
+            var folderResponse = getFolderResponse(folder);
+
+            result.add(folderResponse);
+
+            recursiveInsertFolder(subFolderId, subFolderMap, folderMap, result);
+        }
+    }
+
+    private FolderResponse getFolderResponse(Folder folder) {
+        return FolderResponse.of(folder.getId(), folder.getName());
+    }
 }
---
+++ src/main/java/notai/folder/application/result/FolderResponse.java
@@ -0,0 +1,18 @@
+package notai.folder.application.result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record FolderResponse(
+        Long id,
+        String name,
+        List<FolderResponse> subFolders
+) {
+    public static FolderResponse of(Long id, String name) {
+        return new FolderResponse(
+                id,
+                name,
+                new ArrayList<>()
+        );
+    }
+}
