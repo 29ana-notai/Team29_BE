74909e5fb89c442e6ea1b3ac08236241146189b8
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-09-30 20:47:08 +0900

    feat: Document 생성 API 구현
    Document 생성 API 구현

--- src/main/java/notai/document/presentation/DocumentController.java
+++ src/main/java/notai/document/presentation/DocumentController.java
@@ -1,11 +1,37 @@
 package notai.document.presentation;
 
 import lombok.RequiredArgsConstructor;
+import notai.document.application.DocumentService;
+import notai.document.application.result.DocumentSaveResult;
+import notai.document.presentation.request.DocumentSaveRequest;
+import notai.document.presentation.response.DocumentSaveResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
 
 @Controller
 @RequestMapping("/api/folders/{folderId}/documents")
 @RequiredArgsConstructor
 public class DocumentController {
+
+    private final DocumentService documentService;
+
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<DocumentSaveResponse> saveDocument(
+            @PathVariable Long folderId,
+            @RequestPart MultipartFile pdfFile,
+            @RequestPart DocumentSaveRequest documentSaveRequest
+    ) {
+        DocumentSaveResult documentSaveResult = documentService.saveDocument(folderId, pdfFile, documentSaveRequest);
+        DocumentSaveResponse response = DocumentSaveResponse.from(documentSaveResult);
+        String url = String.format("/api/folders/%s/documents/%s", folderId, response.id());
+        return ResponseEntity.created(URI.create(url)).body(response);
+    }
 }
