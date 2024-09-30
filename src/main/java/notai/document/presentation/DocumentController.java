443b2baf745bd0d90f8e173de89919fd19af9bd9
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-09-30 15:14:06 +0900

    chore: API 매칭 URL 수정 작업 진행
    API 매칭 URL 수정 작업 진행

--- src/main/java/notai/document/presentation/DocumentController.java
+++ src/main/java/notai/document/presentation/DocumentController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/documents")
+@RequestMapping("/api/folders/{folderId}/documents")
 @RequiredArgsConstructor
 public class DocumentController {
 }
