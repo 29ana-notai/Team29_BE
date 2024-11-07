9a1fb7921a8e43bdbc541a920de73d0ce53bfd78
Shsin9797 <aaa8141@naver.com>
2024-11-07 23:21:03 +0900

    Feat: Post 조회 기능 추가 구현

--- src/main/java/notai/post/application/PostService.java
+++ src/main/java/notai/post/application/PostService.java
@@ -10,6 +10,9 @@ import notai.post.domain.Post;
 import notai.post.domain.PostRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -26,5 +29,30 @@ public class PostService {
     public PostFindResult findPost(Long postId) {
         Post post = postRepository.findById(postId).get();
         return PostFindResult.of(post);
+    }
+
+    public List<PostFindResult> findPostAll() {
+        //게시글 조회
+        List<Post> posts = postRepository.findAll();
+        // 엔티티 -> dto 변환
+        List<PostFindResult> postFindResponses = new ArrayList<>();
+        for (int i = 0 ; i < posts.size() ; i++){
+            Post post =posts.get(i);
+            PostFindResult postFindResult = PostFindResult.of(post);
+            postFindResponses.add(postFindResult);
+        }
+        return postFindResponses;
+    }
+
+    public List<PostFindResult> findPostsByMemberId(Long memberId) {
+
+        List<Post> posts = postRepository.findAllByMemberId(memberId);
+        List<PostFindResult> postFindResponses = new ArrayList<>();
+        for (int i = 0 ; i < posts.size() ; i++){
+            Post post =posts.get(i);
+            PostFindResult postFindResult = PostFindResult.of(post);
+            postFindResponses.add(postFindResult);
+        }
+        return postFindResponses;
     }
 }
--- src/main/java/notai/post/presentation/PostController.java
+++ src/main/java/notai/post/presentation/PostController.java
@@ -8,10 +8,13 @@ import notai.post.application.result.PostFindResult;
 import notai.post.application.result.PostSaveResult;
 import notai.post.presentation.request.PostSaveRequest;
 import notai.post.presentation.response.PostFindResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/post")
@@ -22,15 +25,15 @@ public class PostController {
 
     @PostMapping
     public ResponseEntity<Void> savePost(
-            @Auth Long memberId,
-            @RequestBody PostSaveRequest postSaveRequest
+            @Auth Long memberId, @RequestBody PostSaveRequest postSaveRequest
     ) {
         PostSaveCommand postSaveCommand = postSaveRequest.toCommand();
-        PostSaveResult postSaveResult = postService.savePost(memberId,postSaveCommand);
+        PostSaveResult postSaveResult = postService.savePost(memberId, postSaveCommand);
         String url = String.format("/api/post/%s", postSaveResult.id());
         return ResponseEntity.created(URI.create(url)).build();
     }
 
+    //post id로 게시글 1개 조회
     @GetMapping(value = "/{postId}")
     public ResponseEntity<PostFindResponse> getPost(
             @PathVariable Long postId
@@ -38,6 +41,28 @@ public class PostController {
         PostFindResult postFindResult = postService.findPost(postId);
         PostFindResponse response = PostFindResponse.from(postFindResult);
         return ResponseEntity.ok(response);
+    }
+
+    //게시글 전체 조회
+    @GetMapping
+    public ResponseEntity<List<PostFindResponse>> getPosts() {
+        List<PostFindResult> postFindResults = postService.findPostAll();
+        List<PostFindResponse> postFindResponses = postFindResults.stream()
+                                                                  .map(PostFindResponse::from)
+                                                                  .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(postFindResponses);
+    }
+
+    // memberID 에 해당하는 게시글 전체 조회
+    @GetMapping(value = "/{memberId}")
+    public ResponseEntity<List<PostFindResponse>> getPostByMemberId(
+            @Auth Long memberId
+    ) {
+        List<PostFindResult> postFindResults  = postService.findPostsByMemberId(memberId);
+        List<PostFindResponse> postFindResponses = postFindResults.stream()
+                                                                  .map(PostFindResponse::from)
+                                                                  .collect(Collectors.toList());
+        return ResponseEntity.status(HttpStatus.OK).body(postFindResponses);
     }
 
 }
