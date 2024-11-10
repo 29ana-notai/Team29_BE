74e05a9dedf32703c118d784205e1df64c4ff4eb
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-11-10 20:15:15 +0900

    chore: 트랜잭션 어노테이션 추가
    트랜잭션 어노테이션 추가

--- src/main/java/notai/ocr/application/OCRQueryService.java
+++ src/main/java/notai/ocr/application/OCRQueryService.java
@@ -1,23 +1,26 @@
 package notai.ocr.application;
 
 import lombok.RequiredArgsConstructor;
+import static notai.common.exception.ErrorMessages.OCR_RESULT_NOT_FOUND;
 import notai.common.exception.type.NotFoundException;
 import notai.member.domain.Member;
 import notai.ocr.application.result.OCRFindResult;
 import notai.ocr.domain.OCR;
 import notai.ocr.domain.OCRRepository;
 import org.springframework.stereotype.Service;
-
-import static notai.common.exception.ErrorMessages.OCR_RESULT_NOT_FOUND;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OCRQueryService {
 
     private final OCRRepository ocrRepository;
 
     public OCRFindResult findOCR(Member member, Long documentId, Integer pageNumber) {
-        OCR ocr = ocrRepository.findOCRByDocumentIdAndPageNumber(documentId, pageNumber
+        OCR ocr = ocrRepository.findOCRByDocumentIdAndPageNumber(
+                documentId,
+                pageNumber
         ).orElseThrow(() -> new NotFoundException(OCR_RESULT_NOT_FOUND));
         ocr.getDocument().validateOwner(member);
 
